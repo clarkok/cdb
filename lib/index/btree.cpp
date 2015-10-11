b2477f1f21bd087597b7dcf65d43e4a295193020
Clark Zhang <mail@clarkok.com>
2015-10-11 20:18:44 +0800

    btree partical completed

--- lib/index/btree.cpp
+++ lib/index/btree.cpp
@@ -1,51 +1,187 @@
+#include <cassert>
+#include <stack>
+
 #include "btree.hpp"
 
 using namespace cdb;
 
 namespace cdb {
-    class BTree::BTreeNode
+    struct BTree::NodeHeader
     {
-        Slice _content;
-        Length _record_length;
-        bool _is_leaf;
-    public:
-        BTreeNode(Slice content, Length record_length, bool is_leaf);
+        bool node_is_leaf : 1;
+        unsigned int node_length : 15;
+        unsigned int entry_count : 16;
     };
 
-    struct LeafMark
+    struct BTree::NodeMark
     {
+        NodeHeader header;
+        BlockIndex after;
+    };
+
+    struct BTree::LeafMark
+    {
+        NodeHeader header;
         BlockIndex prev;
         BlockIndex next;
-        Length length;
     };
 }
 
-BTree::BTreeNode::BTreeNode(Slice content, Length record_length, bool is_leaf)
-    : _content(content), _record_length(record_length), _is_leaf(is_leaf)
-{ }
-
 BTree::BTree(
         DriverAccesser *accesser,
-        Length record_length,
-        Length record_count,
-        BlockIndex root_index
+        Comparator less,
+        Comparator equal,
+        BlockIndex root_index,
+        Length key_size,
+        Length value_size
     )
     : _accesser(accesser),
-      _record_length(record_length),
-      _record_count(record_count),
-      _root(new BTreeNode(
-              accesser->access(root_index),
-              record_count < maximumRecordPerLeaf() ? record_length : SIZEOF_INDEX,
-              record_count < maximumRecordPerLeaf()
-        ))
+      _less(less),
+      _equal(equal),
+      _root_index(root_index),
+      _root(accesser->aquire(root_index)),
+      _key_size(key_size),
+      _value_size(value_size)
 { }
 
 Length
-BTree::maximumRecordPerLeaf() const
-{ return (Driver::BLOCK_SIZE - sizeof(LeafMark)) / _record_length; }
+BTree::maximumEntryPerNode() const
+{ return (Driver::BLOCK_SIZE - sizeof(NodeMark)) / nodeEntrySize(); }
+
+Length
+BTree::nodeEntrySize() const
+{ return _key_size + sizeof(BlockIndex); }
+
+Length
+BTree::maximumEntryPerLeaf() const
+{ return (Driver::BLOCK_SIZE - sizeof(LeafMark)) / leafEntrySize(); }
+
+Length
+BTree::leafEntrySize() const
+{ return _key_size + _value_size; }
 
 void
 BTree::reset()
 {
     // TODO
+}
+
+BTree::Iterator
+BTree::find(ConstSlice key)
+{
+    std::stack<Block> path;
+
+    keepTracingToLeaf(key, path);
+
+    return findInLeaf(path.top(), key);
+}
+
+void
+BTree::keepTracingToLeaf(ConstSlice key, std::stack<Block> &path)
+{
+    assert(key.length() >= _key_size);
+    path.push(_accesser->aquire(_root_index));
+
+    auto *header = getHeaderFromNode(path.top());
+
+    while (!header->node_is_leaf) {
+        path.push(_accesser->aquire(findInNode(path.top(), key)));
+        header = getHeaderFromNode(path.top());
+    }
+}
+
+BTree::NodeHeader *
+BTree::getHeaderFromNode(Slice node)
+{ return reinterpret_cast<NodeHeader*>(node.content()); }
+
+BlockIndex
+BTree::findInNode(Block &node, ConstSlice key)
+{
+    auto *mark = getMarkFromNode(node);
+    auto *entry_limit = node.begin() + mark->header.entry_count * nodeEntrySize();
+
+    auto *entry = getFirstEntryInNode(node);
+
+    /* Node Entries
+     *
+     * | key | index |
+     *
+     * all keys in block by `index` less than `key`
+     */
+
+    for (; entry < entry_limit; entry = nextEntryInNode(entry)) {
+        if (_less(key.content(), getKeyFromNodeEntry(entry))) {
+            return getIndexFromNodeEntry(entry);
+        }
+    }
+    return mark->after;
+}
+
+BTree::NodeMark *
+BTree::getMarkFromNode(Slice node)
+{ return reinterpret_cast<NodeMark*>(node.content()); }
+
+Byte *
+BTree::getFirstEntryInNode(Slice node)
+{ return node.content() + sizeof(NodeMark); }
+
+Byte *
+BTree::nextEntryInNode(Byte *entry)
+{ return entry + nodeEntrySize(); }
+
+Byte *
+BTree::getKeyFromNodeEntry(Byte *entry)
+{ return entry; }
+
+BlockIndex
+BTree::getIndexFromNodeEntry(Byte *entry)
+{ return *reinterpret_cast<BlockIndex*>(entry + _key_size); }
+
+BTree::Iterator
+BTree::findInLeaf(Block &leaf, ConstSlice key)
+{
+    auto *mark = getMarkFromLeaf(leaf);
+    auto *entry_limit = leaf.begin() + mark->header.entry_count * leafEntrySize();
+
+    auto *entry = getFirstEntryInLeaf(leaf);
+
+    for (; entry < entry_limit; entry = nextEntryInLeaf(entry)) {
+        if (_equal(getKeyFromLeafEntry(entry), key.content())) {
+            return Iterator(this, leaf, entry - leaf.begin());
+        }
+    }
+
+    // TODO throw exception
+    throw 1;
+}
+
+BTree::LeafMark *
+BTree::getMarkFromLeaf(Slice leaf)
+{ return reinterpret_cast<LeafMark*>(leaf.content()); }
+
+Byte *
+BTree::getFirstEntryInLeaf(Slice leaf)
+{ return leaf.content() + sizeof(LeafMark); }
+
+Byte *
+BTree::nextEntryInLeaf(Byte *entry)
+{ return entry + leafEntrySize(); }
+
+Byte *
+BTree::getKeyFromLeafEntry(Byte *entry)
+{ return entry; }
+
+Slice
+BTree::getValueFromLeafEntry(Byte *entry)
+{ return Slice(entry + _key_size, _value_size); }
+
+BTree::Iterator
+BTree::insert(ConstSlice key, ConstSlice value)
+{
+    assert(key.length() >= _key_size);
+}
+
+Slice
+BTree::splitLeaf(Slice leaf, BlockIndex &new_index, ConstSlice key, ConstSlice value)
+{
 }
--- lib/index/btree.hpp
+++ lib/index/btree.hpp
@@ -1,6 +1,7 @@
 #ifndef _DB_INDEX_BTREE_H_
 #define _DB_INDEX_BTREE_H_
 
+#include <stack>
 #include <iterator>
 
 #include "lib/driver/driver-accesser.hpp"
@@ -10,32 +11,123 @@ namespace cdb {
     class BTree
     {
     public:
-        struct iterator : public std::iterator<std::bidirectional_iterator_tag, Slice>
-        { };
+        class Iterator
+        {
+        private:
+            BTree *_owner;
+            Block _block;
+            Length _offset;
 
-        struct const_iterator : public std::iterator<std::bidirectional_iterator_tag, ConstSlice>
-        { };
+            Iterator(BTree *owner, const Block &block, Length offset)
+                : _owner(owner), _block(block), _offset(offset)
+            { }
+
+            Iterator(const Iterator &) = delete;
+            Iterator &operator = (const Iterator &) = delete;
+
+            inline Byte *
+            getEntry() const
+            { return _block.slice().content() + _offset; }
+
+            friend class BTree;
+        public:
+            Iterator(Iterator &&iter)
+                : _owner(iter._owner), _block(std::move(iter._block)), _offset(iter._offset)
+            { }
+
+            ~Iterator() = default;
+
+            inline bool
+            operator == (const Iterator &iter)
+            { 
+                return _owner == iter._owner &&
+                     _block.index() == iter._block.index() &&
+                     _offset == iter._offset;
+            }
+
+            inline bool
+            operator != (const Iterator &iter)
+            { return !this->operator==(iter); }
+
+            Iterator &inc();
+            Iterator &dec();
+
+            inline const Byte *
+            getKey() const
+            { return _owner->getKeyFromLeafEntry(getEntry()); }
+
+            Slice getValue() const
+            { return _owner->getValueFromLeafEntry(getEntry()); }
+        };
+
+        typedef bool (*Comparator)(const Byte *, const Byte *);
 
     private:
-        class BTreeNode;
-
-        static const auto SIZEOF_INDEX = sizeof(BlockIndex);
+        struct NodeHeader;
+        struct NodeMark;
+        struct LeafMark;
 
         DriverAccesser *_accesser;
-        Length _record_length;
-        Length _record_count;
-        std::unique_ptr<BTreeNode> _root;
+        Comparator _less;
+        Comparator _equal;
 
-        Length maximumRecordPerLeaf() const;
+        BlockIndex _root_index;
+        Block _root;
+
+        Length _key_size;
+        Length _value_size;
+
+        inline Length nodeEntrySize() const;
+        inline Length leafEntrySize() const;
+
+        inline Length maximumEntryPerNode() const;
+        inline Length maximumEntryPerLeaf() const;
+
+        inline NodeMark *getMarkFromNode(Slice node);
+        inline LeafMark *getMarkFromLeaf(Slice leaf);
+        inline NodeHeader *getHeaderFromNode(Slice node);
+
+        inline Byte *getFirstEntryInNode(Slice node);
+        inline Byte *nextEntryInNode(Byte *entry);
+
+        inline Byte *getFirstEntryInLeaf(Slice leaf);
+        inline Byte *nextEntryInLeaf(Byte *entry);
+
+        inline BlockIndex getIndexFromNodeEntry(Byte *entry);
+        inline Slice      getValueFromLeafEntry(Byte *entry);
+
+        inline Byte* getKeyFromNodeEntry(Byte *entry);
+        inline Byte* getKeyFromLeafEntry(Byte *entry);
+
+        inline BlockIndex findInNode(Block &node, ConstSlice key);
+        inline Iterator   findInLeaf(Block &leaf, ConstSlice key);
+
+        inline void keepTracingToLeaf(ConstSlice key, std::stack<Block> &path);
+
+        /**
+         * @return new key to split
+         */
+        inline Slice splitLeaf(Slice leaf, BlockIndex &new_index, ConstSlice key, ConstSlice value);
+        inline Slice splitNode(Slice node, BlockIndex &new_index, ConstSlice key, BlockIndex index);
+
+        inline void insertInLeaf(Slice leaf, ConstSlice key, ConstSlice value);
+        inline void insertInNode(Slice node, ConstSlice key, BlockIndex index);
+
+        inline BlockIndex newRoot(ConstSlice split_key, BlockIndex before, BlockIndex after);
 
     public:
         BTree(
                 DriverAccesser *accesser,
-                Length record_length,
-                Length record_count,
-                BlockIndex root_index
+                Comparator less,
+                Comparator equal,
+                BlockIndex root_index,
+                Length key_size,
+                Length value_size
             );
         ~BTree();
+
+        Iterator find(ConstSlice key);
+        Iterator insert(ConstSlice key, ConstSlice value);
 
         void reset();
     };
